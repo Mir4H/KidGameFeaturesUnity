9615ab04cedf837a37b96d9710e03476b29ae016
Mira Hamalainen <mira.m.hamalainen@gmail.com>
2023-07-02 09:42:27 +0300

    drag and drop feature done

---
+++ Assets/Scripts/DragDrop/PuzzlePiece.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip _pickUpClip, _dropClip;
+    private bool _dragging, _placed;
+    private Vector2 _offset, _originalPosition;
+    Camera mainCamera;
+    private PuzzleSlot _slot;
+
+    public void Init(PuzzleSlot slot)
+    {
+        _renderer.sprite = slot.Renderer.sprite;
+        _slot = slot;
+    }
+
+    private void Awake()
+    {
+        _originalPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (_placed) return;
+        if(!_dragging) return;
+        var mousePosition = GetMousePos();
+
+        transform.position = mousePosition - _offset;
+    }
+
+    private void OnMouseDown()
+    {
+        _dragging = true;
+        _source.PlayOneShot(_pickUpClip);
+
+        _offset = GetMousePos() - (Vector2)transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        if(Vector2.Distance(transform.position, _slot.transform.position) < 3)
+        {
+            transform.position = _slot.transform.position;
+            _slot.Placed();
+            _placed = true;
+        }
+        else
+        {
+            transform.position = _originalPosition;
+            _dragging = false;
+            _source.PlayOneShot(_dropClip);
+        }  
+    }
+
+    Vector2 GetMousePos()
+    {
+        return mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
